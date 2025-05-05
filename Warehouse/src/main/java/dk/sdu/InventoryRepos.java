2303cfd3f76662a19b03c43ef61d4b1c1d661daa
Emilie Kildemoes <emkil21@student.sdu.dk>
2025-05-05 10:23:46 +0200

    Inventory sat op med metoder + controller ændret

--- Warehouse/src/main/java/dk/sdu/InventoryRepos.java
+++ Warehouse/src/main/java/dk/sdu/InventoryRepos.java
@@ -1,4 +1,9 @@
 package dk.sdu;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class InventoryRepos {
+import java.util.Optional;
+
+//Spring interface der giver os CRUD operations
+public interface InventoryRepos extends JpaRepository<InventoryItems, Long> {
+    Optional<InventoryItems> findByTrayId(int trayId);
 }
--- Warehouse/src/main/java/dk/sdu/Service/soapWarehouseService.java
+++ Warehouse/src/main/java/dk/sdu/Service/soapWarehouseService.java
@@ -2,34 +2,57 @@ package dk.sdu.Service;
 
 import com.example.generated.IEmulatorService;
 import com.example.generated.IEmulatorService_Service;
+import dk.sdu.InventoryItems;
+import dk.sdu.InventoryRepos;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+//connector vores repository til resten af applikationen
 @Service
 public class soapWarehouseService {
-
+    private final InventoryRepos inventoryRepos;
     private final IEmulatorService servicePort;
 
-    public soapWarehouseService() {
+    public soapWarehouseService(InventoryRepos inventoryRepos) {
+        this.inventoryRepos = inventoryRepos;
         IEmulatorService_Service service = new IEmulatorService_Service();
         this.servicePort = service.getBasicHttpBindingIEmulatorService();
     }
 
-    public soapWarehouseService(IEmulatorService servicePort) {
+
+    public soapWarehouseService(InventoryRepos inventoryRepos, IEmulatorService servicePort) {
+        this.inventoryRepos = inventoryRepos;
         this.servicePort = servicePort;
     }
 
-    //returnere en string som er inventory
-    public String getInventory() {
-        return servicePort.getInventory();
+    //returnere inventory
+    public List<InventoryItems> getInventory(){
+        return inventoryRepos.findAll();
     }
 
     //Handler at kunne indsætte items på trays
-    public String insertItem(int trayId, String itemName) {
-        return servicePort.insertItem(trayId, itemName);
+    public InventoryItems insertItem(int trayId, String itemName){
+        InventoryItems item = new InventoryItems();
+        item.setTrayId(trayId);
+        item.setItemName(itemName);
+        item.setQuantity(1);
+        return inventoryRepos.save(item);
     }
+
 
     //Kan finde/fjerne inventory i bestemt tray
     public String pickItem(int trayId) {
-        return servicePort.pickItem(trayId);
+        InventoryItems item = inventoryRepos.findByTrayId(trayId)
+                .orElseThrow(()-> new RuntimeException("Item not found"));
+        item.setQuantity(item.getQuantity()-1);
+        if(item.getQuantity()<= 0){
+            inventoryRepos.delete(item);
+            return "Item picked and tray now empty";
+        } else{
+            inventoryRepos.save(item);
+            return "Item picked, remaining quantity: "+ item.getQuantity();
+        }
     }
 }
