056c12882aecd0a06400dcde60d64ab96836ae8f
Benjamin Senika Hansen <143800494+Bsen1ka@users.noreply.github.com>
2025-05-08 13:01:20 +0200

    Database's working now

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ST4-semprojekt-gr6
+
--- Warehouse/src/main/java/dk/sdu/Main.java
+++ Warehouse/src/main/java/dk/sdu/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-@SpringBootApplication()
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
