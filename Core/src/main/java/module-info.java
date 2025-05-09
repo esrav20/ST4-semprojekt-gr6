90c767b8d8df595cd2d8ac52060bcffd03bfd3ed
esrav20 <esrav20@student.sdu.dk>
2025-05-09 10:19:42 +0200

    Branches: AGV, Warehouse, AssemblyStation merged

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -3,6 +3,9 @@ module Core {
     requires javafx.fxml;
     requires AGV;
     requires CommonAGV;
+    requires CommonAssemblyStation;
+    requires org.eclipse.paho.client.mqttv3;
+    requires AssemblyStation;
 
     opens com.example.guidemo_4semester to javafx.fxml;
     exports com.example.guidemo_4semester;
