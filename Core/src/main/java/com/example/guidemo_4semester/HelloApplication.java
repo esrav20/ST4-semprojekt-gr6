6ddce927af1a77937aa9dea2d5358b7a825aac47
esrav20 <esrav20@student.sdu.dk>
2025-05-11 17:08:54 +0200

    forsøger at sætte Mqtt op i TabViewController

--- Core/src/main/java/com/example/guidemo_4semester/HelloApplication.java
+++ Core/src/main/java/com/example/guidemo_4semester/HelloApplication.java
@@ -1,12 +1,14 @@
 package com.example.guidemo_4semester;
 
 import dk.sdu.AGV.AGVMovement;
+import dk.sdu.Common.IMqttService;
 import dk.sdu.CommonAGV.AGVPI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,11 +31,15 @@ public class HelloApplication extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, MqttException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/views/TabView.fxml"));
         fxmlLoader.setControllerFactory(springContext::getBean);
         Parent root = fxmlLoader.load();
+
+        //Injecting non-Spring dependencies:
+        TabViewController controller = fxmlLoader.getController();
+        controller.setDependencies(springContext.getBean(AGVPI.class), springContext.getBean(IMqttService.class));
 
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
