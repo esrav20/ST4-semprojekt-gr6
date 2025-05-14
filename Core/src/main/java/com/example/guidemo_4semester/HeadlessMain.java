af6dbea6f7739c2a07cf24787986e0c6479fe10f
Benjamin Senika Hansen <143800494+Bsen1ka@users.noreply.github.com>
2025-05-14 14:22:31 +0200

    Database's working now

---
+++ Core/src/main/java/com/example/guidemo_4semester/HeadlessMain.java
@@ -0,0 +1,11 @@
+package com.example.guidemo_4semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlessMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HeadlessMain.class, args);
+    }
+}
