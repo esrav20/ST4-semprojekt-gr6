91eb5f835c87020b3de17941aedec6f42fe3d813
Olsgaard1 <chols23@student.sdu.dk>
2025-05-16 22:38:03 +0200

    Grinden, alt burde virke i inventory tab nu :O

--- Core/src/main/java/com/example/guidemo_4semester/AddItemController.java
+++ Core/src/main/java/com/example/guidemo_4semester/AddItemController.java
@@ -1,6 +1,7 @@
 package com.example.guidemo_4semester;
 
 import dk.sdu.CommonAGV.AGVPI;
+import dk.sdu.CommonInventory.InventoryView;
 import dk.sdu.CommonInventory.WarehousePI;
 import javafx.fxml.FXML;
 import javafx.fxml.FXML;
@@ -9,12 +10,16 @@ import javafx.stage.Stage;
 
 public class AddItemController {
     private final WarehousePI warehouseClient;
+    private Runnable onSubmitSuccess;
+
+    public void setOnSubmitSuccess(Runnable onSubmitSuccess) {
+        this.onSubmitSuccess = onSubmitSuccess;
+    }
 
     public AddItemController(WarehousePI warehouseClient) {
         this.warehouseClient = warehouseClient;
     }
 
-        @FXML private TextField Tray_ID;
         @FXML private TextField Amount;
         @FXML private TextField Item;
 
@@ -22,21 +27,28 @@ public class AddItemController {
         private void handleSubmit() {
             String name = Item.getText();
             String amount = Amount.getText();
-            String Trayid = Tray_ID.getText();
-
-
 
             System.out.println("Name: " + name);
             System.out.println("Amount: " + amount);
-            System.out.println("Trayid: " + Trayid);
 
-            warehouseClient.insertItem(Integer.parseInt(Trayid), name, Integer.parseInt(amount));
+            int nextTrayId = warehouseClient.getInventory().stream()
+                    .mapToInt(InventoryView::getTrayId)
+                    .max()
+                    .orElse(0) + 1;
+
+
+            warehouseClient.insertItem(nextTrayId, name, Integer.parseInt(amount));
 
             System.out.println(warehouseClient.getInventory());
+            if(onSubmitSuccess != null) {
+                onSubmitSuccess.run();
+            }
 
             Stage stage = (Stage) Item.getScene().getWindow();
             stage.close();
 
+
         }
+
     }
 
--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -21,6 +21,7 @@ module Core {
     requires CommonAGV;
     requires CommonAssemblyStation;
     requires java.desktop;
+    requires org.hibernate.orm.core;
     opens com.example.guidemo_4semester.Queue to javafx.base;
     opens com.example.guidemo_4semester to javafx.fxml, spring.core, spring.beans, spring.context;
     //opens views to javafx.fxml, spring.core, spring.beans, spring.context;
